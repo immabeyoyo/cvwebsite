9b1069bd959419896cf33e828a3e00001002acb2
immabeyoyo <debruinyorick@gmail.com>
2023-12-21 09:36:37 +0100

    index, script, styles

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt("Wat is a?");
+let b = prompt("Wat is b?");
+let c = a * b;
+let uitkomstDiv = document.createElement("div");
+
+uitkomstDiv.innerHTML = c;
+uitkomstDiv.id = "uitkomst";
+
+document.body.appendChild(uitkomstDiv);
