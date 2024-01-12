838db8c7aef4cedc63c11172f9c351457cbe8b19
Kaiskyy <zfleminks@hotmail.com>
2024-01-12 17:37:24 +0100

    alle html die jullie in javascript hadden gemaakt overgezet naar de html bestanden voor beter overzicht.

--- homescript.js
+++ homescript.js
@@ -1,31 +1,4 @@
-// Function to load the index.html page
-function loadIndexPagina() {
-  window.location.href = 'index.html';
-}
 
-// Create the header element
-let header = document.createElement('div');
-
-let homePaginaKop = document.createElement('p');
-homePaginaKop.innerHTML = 'Welkom bij CV maker. <br> Hier kan je je gegevens invullen en deze invullen in een gekozen cv layout. <br> Klik op de knop om verder te gaan.';
-
-let logo = document.createElement('img');
-logo.src = 'Logo.png';
-
-// Create the "Volgende pagina" button
-let volgendePaginaButton = document.createElement('button');
-volgendePaginaButton.textContent = 'Volgende pagina';
-volgendePaginaButton.addEventListener('click', loadIndexPagina);
-
-let advertentie = document.createElement('div');
-advertentie.id = 'advertentie'
-advertentie.innerHTML = 'Uw advertentie hier? <br> Dat kan! Neem nu contact op. <br> michiel.vanwijk@student.vonknh.nl'
-
-// Append the button to the header
-document.body.appendChild(logo);
-header.appendChild(volgendePaginaButton);
-
-// Append the header to the body
-document.body.appendChild(homePaginaKop);
-document.body.appendChild(header);
-document.body.appendChild(advertentie);
+function nextpage() {
+  window.location.href = "index.html"
+  }
