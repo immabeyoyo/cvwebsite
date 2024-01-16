47ca2f743d931c4f405edbcac2bb5343d986da71
Kaiskyy <136729968+Kaiskyy@users.noreply.github.com>
2024-01-16 10:55:37 +0100

    Add files via upload

--- homescript.js
+++ homescript.js
@@ -2,3 +2,11 @@
 function nextpage() {
   window.location.href = "index.html"
   }
+
+  function accountMaken() {
+    window.location.href = "AccountMaken.html"
+    }
+    
+    function Inloggen() {
+    window.location.href = "Inloggen.html"
+    }
---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://bmvgowvyvnyvohpzuhfm.supabase.co', 'public-anon-key')
