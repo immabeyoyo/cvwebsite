964d7157c46526491bd5f44889caae136c2f19f5
immabeyoyo <debruinyorick@gmail.com>
2024-02-05 14:51:05 +0100

    Update cvkiezen.js

--- cvkiezen.js
+++ cvkiezen.js
@@ -1,11 +1,11 @@
 function kiesCV1 () {
-  window.location.href = "home.html"
+  window.location.href = "cv1.html"
 }
 
 function kiesCV2 () {
-  window.location.href = "home.html"
+  window.location.href = "cv2.html"
 }
 
 function kiesCV3 () {
-  window.location.href = "home.html"
+  window.location.href = "cv3.html"
 }
