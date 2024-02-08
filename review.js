f670cc72384a752bc20a539731b2d12d16b48d54
Yorick <145762996+immabeyoyo@users.noreply.github.com>
2024-02-08 09:37:05 +0100

    Add files via upload

--- review.js
+++ review.js
@@ -1,80 +1,15 @@
 function uploadreview() {
-    document.querySelector("#reviewmaker #reviewtitle").contentEditable = 'false';
-    document.querySelector("#reviewmaker #review").contentEditable = 'false';
+    console.log("Disabling contentEditable...");
+    document.querySelector("#revieuwmaker #revieuwtitle").contentEditable = 'false';
+    document.querySelector("#revieuwmaker #revieuw").contentEditable = 'false';
 
-    var sourceElement = document.getElementById("reviewmaker");
+    var sourceElement = document.getElementById("revieuwmaker");
     var clonedElement = sourceElement.cloneNode(true);
     clonedElement.id = "submitted";
-    var targetElement = document.getElementById('reviews');
+    var targetElement = document.getElementById('revieuws');
     targetElement.appendChild(clonedElement);
 
-    document.querySelector("#reviewmaker #reviewtitle").contentEditable = 'true';
-    document.querySelector("#reviewmaker #review").contentEditable = 'true';
-    document.querySelector("#submitted #star1").disabled = true;
-    document.querySelector("#submitted #star2").disabled = true;
-    document.querySelector("#submitted #star3").disabled = true;
-    document.querySelector("#submitted #star4").disabled = true;
-    document.querySelector("#submitted #star5").disabled = true;
+    document.querySelector("#revieuwmaker #revieuwtitle").contentEditable = 'true';
+    document.querySelector("#revieuwmaker #revieuw").contentEditable = 'true';
 
-}
-function button1(){
-    console.log("Button 1 pressed");
-    const button1 = document.querySelector("#reviewmaker #star1")
-    button1.style.backgroundImage = "url('star (1).png')";
-    const button2 = document.querySelector("#reviewmaker #star2")
-    button2.style.backgroundImage = "url('star.png')";
-    const button3 = document.querySelector("#reviewmaker #star3")
-    button3.style.backgroundImage = "url('star.png')";
-    const button4 = document.querySelector("#reviewmaker #star4")
-    button4.style.backgroundImage = "url('star.png')";
-    const button5 = document.querySelector("#reviewmaker #star5")
-    button5.style.backgroundImage = "url('star.png')";
-}
-function button2(){
-    const button1 = document.querySelector("#reviewmaker #star1")
-    button1.style.backgroundImage = "url('star (1).png')";
-    const button2 = document.querySelector("#reviewmaker #star2")
-    button2.style.backgroundImage = "url('star (1).png')";
-    const button3 = document.querySelector("#reviewmaker #star3")
-    button3.style.backgroundImage = "url('star.png')";
-    const button4 = document.querySelector("#reviewmaker #star4")
-    button4.style.backgroundImage = "url('star.png')";
-    const button5 = document.querySelector("#reviewmaker #star5")
-    button5.style.backgroundImage = "url('star.png')";
-}
-function button3(){
-    const button1 = document.querySelector("#reviewmaker #star1")
-    button1.style.backgroundImage = "url('star (1).png')";
-    const button2 = document.querySelector("#reviewmaker #star2")
-    button2.style.backgroundImage = "url('star (1).png')";
-    const button3 = document.querySelector("#reviewmaker #star3")
-    button3.style.backgroundImage = "url('star (1).png')";
-    const button4 = document.querySelector("#reviewmaker #star4")
-    button4.style.backgroundImage = "url('star.png')";
-    const button5 = document.querySelector("#reviewmaker #star5")
-    button5.style.backgroundImage = "url('star.png')";
-}
-function button4(){
-    const button1 = document.querySelector("#reviewmaker #star1")
-    button1.style.backgroundImage = "url('star (1).png')";
-    const button2 = document.querySelector("#reviewmaker #star2")
-    button2.style.backgroundImage = "url('star (1).png')";
-    const button3 = document.querySelector("#reviewmaker #star3")
-    button3.style.backgroundImage = "url('star (1).png')";
-    const button4 = document.querySelector("#reviewmaker #star4")
-    button4.style.backgroundImage = "url('star (1).png')";
-    const button5 = document.querySelector("#reviewmaker #star5")
-    button5.style.backgroundImage = "url('star.png')";
-}
-function button5(){
-    const button1 = document.querySelector("#reviewmaker #star1")
-    button1.style.backgroundImage = "url('star (1).png')";
-    const button2 = document.querySelector("#reviewmaker #star2")
-    button2.style.backgroundImage = "url('star (1).png')";
-    const button3 = document.querySelector("#reviewmaker #star3")
-    button3.style.backgroundImage = "url('star (1).png')";
-    const button4 = document.querySelector("#reviewmaker #star4")
-    button4.style.backgroundImage = "url('star (1).png')";
-    const button5 = document.querySelector("#reviewmaker #star5")
-    button5.style.backgroundImage = "url('star (1).png')";
 }
