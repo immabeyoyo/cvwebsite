fe5b148dafb3baf80bba21fb4df6853e51bc4d8b
immabeyoyo <debruinyorick@gmail.com>
2024-02-08 12:07:43 +0100

    yesyes

--- fetchtest.js
+++ fetchtest.js
@@ -1,23 +1,23 @@
 import { supabase } from './supabase.js';
 
-try {
-  let { data: CVTable1, error } = await supabase
-  .from('CVTable1')
-  .select('*')
+  try {
+    let { data: CVTable1, error } = await supabase
+    .from('CVTable1')
+    .select('*')
 
-if (error) {
-  console.error('Error met data ophalen', error);
-} else {
-  const dataContainer = document.getElementById('div1');
+  if (error) {
+    console.error('Error met data ophalen', error);
+  } else {
+    const dataContainer = document.getElementById('div1');
 
-  dataContainer.innerHTML = '';
+    dataContainer.innerHTML = '';
 
-  CVTable1.forEach(item => {
-    const listItem = document.createElement('div');
-    listItem.textContent = JSON.stringify(item);
-    dataContainer.appendChild(listItem);
-  });
-}
-} catch (err) {
-  console.error('An error occurred:', err);
-}
+    CVTable1.forEach(item => {
+      const listItem = document.createElement('div');
+      listItem.textContent = JSON.stringify(item);
+      dataContainer.appendChild(listItem);
+    });
+  }
+  } catch (err) {
+    console.error('An error occurred:', err);
+  }
