1a2bc3dbe7741575b82568ffc2c592afcb05e6fd
Michielvwijk <michiel.vanwijk@student.vonknh.nl>
2024-02-08 08:59:44 +0100

    review stars enzo

--- review.js
+++ review.js
@@ -13,3 +13,64 @@ function uploadreview() {
     document.querySelector("#revieuwmaker #revieuw").contentEditable = 'true';
 
 }
+function button1(){
+    console.log("Button 1 pressed");
+    const button1 = document.getElementById('star1');
+    button1.style.backgroundImage = "url('star (1).png')";
+    const button2 = document.getElementById('star2');
+    button2.style.backgroundImage = "url('star.png')";
+    const button3 = document.getElementById('star3');
+    button3.style.backgroundImage = "url('star.png')";
+    const button4 = document.getElementById('star4')
+    button4.style.backgroundImage = "url('star.png')";
+    const button5 = document.getElementById('star5')
+    button5.style.backgroundImage = "url('star.png')";
+}
+function button2(){
+    const button1 = document.getElementById('star1');
+    button1.style.backgroundImage = "url('star (1).png')";
+    const button2 = document.getElementById('star2');
+    button2.style.backgroundImage = "url('star (1).png')";
+    const button3 = document.getElementById('star3');
+    button3.style.backgroundImage = "url('star.png')";
+    const button4 = document.getElementById('star4')
+    button4.style.backgroundImage = "url('star.png')";
+    const button5 = document.getElementById('star5')
+    button5.style.backgroundImage = "url('star.png')";
+}
+function button3(){
+    const button1 = document.getElementById('star1');
+    button1.style.backgroundImage = "url('star (1).png')";
+    const button2 = document.getElementById('star2');
+    button2.style.backgroundImage = "url('star (1).png')";
+    const button3 = document.getElementById('star3');
+    button3.style.backgroundImage = "url('star (1).png')";
+    const button4 = document.getElementById('star4')
+    button4.style.backgroundImage = "url('star.png')";
+    const button5 = document.getElementById('star5')
+    button5.style.backgroundImage = "url('star.png')";
+}
+function button4(){
+    const button1 = document.getElementById('star1');
+    button1.style.backgroundImage = "url('star (1).png')";
+    const button2 = document.getElementById('star2');
+    button2.style.backgroundImage = "url('star (1).png')";
+    const button3 = document.getElementById('star3');
+    button3.style.backgroundImage = "url('star (1).png')";
+    const button4 = document.getElementById('star4')
+    button4.style.backgroundImage = "url('star (1).png')";
+    const button5 = document.getElementById('star5')
+    button5.style.backgroundImage = "url('star.png')";
+}
+function button5(){
+    const button1 = document.getElementById('star1');
+    button1.style.backgroundImage = "url('star (1).png')";
+    const button2 = document.getElementById('star2');
+    button2.style.backgroundImage = "url('star (1).png')";
+    const button3 = document.getElementById('star3');
+    button3.style.backgroundImage = "url('star (1).png')";
+    const button4 = document.getElementById('star4')
+    button4.style.backgroundImage = "url('star (1).png')";
+    const button5 = document.getElementById('star5')
+    button5.style.backgroundImage = "url('star (1).png')";
+}
